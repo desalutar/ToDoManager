139a8bc84305bcfb6e676058863c978738847567
Александр Фадеев <afadeev3@alfabank.ru>
2023-06-06 20:08:16 +0300

    fixed cell layout

--- ToDoManager-V2/Controller/TableViewController.swift
+++ ToDoManager-V2/Controller/TableViewController.swift
@@ -64,6 +64,14 @@ class TableViewController: UITableViewController {
         }
     }
     
+//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        UITableView.automaticDimension
+//    }
+//
+//    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+//        44.0
+//    }
+    
 }
 
 
